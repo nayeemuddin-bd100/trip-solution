ff89db9817223a1386a627e9eb8ecea9e45e2116
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-12-12 03:31:17 +0600

    feat: complete Navbar UI

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const Logo = () => {
+
+  // const router = useRouter()
+
+  return (
+			<Image
+				alt="Logo"
+				className="hidden md:block cursor-pointer"
+				height="80"
+				width="80"
+				src="/images/logo.png"
+      />
+	);
+}
+
+export default Logo;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="text-red-500 text-center">
+    <main className="text-red-500 text-center text-2xl">
       <h2> hello world</h2>
     </main>
   )
