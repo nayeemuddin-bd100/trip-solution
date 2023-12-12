097d8267d95f5b754e5fa0d937976038ddcad9eb
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-12-12 13:57:17 +0600

    fix: resolve next js hydration error

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+interface clientOnlyProps {
+  children: React.ReactNode
+}
+
+const ClientOnly = ({children}: clientOnlyProps) => {
+
+  const [hasMounted, setHasMounted] = useState(false)
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+  
+  if(!hasMounted) {
+    return null
+  }
+    return (
+        <>
+           {children} 
+        </>
+    );
+}
+
+export default ClientOnly;
