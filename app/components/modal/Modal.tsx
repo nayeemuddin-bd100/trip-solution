88767948e9330906bf82e15d9ab1f6362b69f7a7
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-12-12 19:40:14 +0600

    feat: complete register modal Ui

--- app/components/modal/Modal.tsx
+++ app/components/modal/Modal.tsx
@@ -67,7 +67,7 @@ const Modal = ({
 	return (
 		<>
 			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-				<div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+				<div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-screen">
 					{/* Content */}
 					<div
 						className={`translate duration-300 h-full ${
@@ -87,7 +87,7 @@ const Modal = ({
 							</div>
 
 							{/* Body */}
-							<div className="relative p-6 flex-auto"></div>
+							<div className="relative p-6 flex-auto">{body}</div>
 
 							{/* Footer */}
 							<div className="flex flex-col gap-2 p-6">
@@ -106,6 +106,7 @@ const Modal = ({
 										label={actionLabel}
 									/>
 								</div>
+								{footer}
 							</div>
 						</div>
 					</div>
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,9 +4,12 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useState } from "react";
 import MenuItem from "./MenuItem";
+import RegisterModal from './../modal/RegisterModal';
+import useRegisterModal from "../hooks/useRegisterModal";
 
 const UserMenu = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const registerModal = useRegisterModal()
 	const toggleOpen = () => {
 		setIsOpen((value) => !value);
 	};
@@ -37,7 +40,7 @@ const UserMenu = () => {
 				<div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
 					<div>
 						<MenuItem onClick={() => {}} label="Login" />
-						<MenuItem onClick={() => {}} label="Sign up" />
+						<MenuItem onClick={registerModal.onOpen} label="Sign up" />
 					</div>
 				</div>
 			)}
