221093c51b0473a18499a9ca6f2072bd01e9a1f0
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-12-26 16:23:52 +0600

    feat: next auth route add

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,67 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/app/libs/prismadb"
+import bcrypt from "bcrypt";
+
+const authOptions: AuthOptions = {
+	providers: [
+		GitHubProvider({
+			clientId: process.env.GITHUB_ID as string,
+			clientSecret: process.env.GITHUB_SECRET as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "text", placeholder: "test@gmail.com" },
+				password: { label: "Password", type: "password" },
+			},
+      async authorize(credentials) {
+        
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        )
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user
+
+
+			},
+		}),
+  ],
+  
+  pages : {
+    signIn : "/"
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
