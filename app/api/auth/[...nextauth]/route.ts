8396c8e68d812955ce620d598b1f7a8f95ca1744
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-12-27 02:24:03 +0600

    fix: add prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,11 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/app/libs/prismadb"
 import bcrypt from "bcrypt";
 
+import type { Adapter } from "next-auth/adapters";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
 export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma) as Adapter,
 	providers: [
 		GitHubProvider({
 			clientId: process.env.GITHUB_ID as string,
@@ -21,47 +25,45 @@ export const authOptions: AuthOptions = {
 				email: { label: "Email", type: "text", placeholder: "test@gmail.com" },
 				password: { label: "Password", type: "password" },
 			},
-      async authorize(credentials) {
-        
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials");
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        })
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error("Invalid credentials");
+				}
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				});
 
-        if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials");
-        }
+				if (!user || !user?.hashedPassword) {
+					throw new Error("Invalid credentials");
+				}
 
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        )
+				const isCorrectPassword = await bcrypt.compare(
+					credentials.password,
+					user.hashedPassword
+				);
 
-        if (!isCorrectPassword) {
-          throw new Error("Invalid credentials");
-        }
+				if (!isCorrectPassword) {
+					throw new Error("Invalid credentials");
+				}
 
-        return user
-
-
+				return user;
 			},
 		}),
-  ],
-  
-  pages : {
-    signIn : "/"
-  },
-  debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt"
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+	],
+
+	pages: {
+		signIn: "/",
+	},
+	// debug: process.env.NODE_ENV === "development",
+	session: {
+		strategy: "jwt",
+	},
+	secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
+
