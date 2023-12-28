a49e702c388b17a59bb4c07eec67f7541436f7f4
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-12-28 13:20:12 +0600

    chore: update image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: [`avatars.githubusercontent.com`,`lh3.googleusercontent.com`],
+		// domains: [
+		// 	`avatars.githubusercontent.com`,
+		// 	`lh3.googleusercontent.com`,
+		// ],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
+		],
 	},
 };
 
