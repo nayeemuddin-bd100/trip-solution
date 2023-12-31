3e979d7b38e3a26ee4c7249587ef9cad370ad6b1
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2024-01-01 00:33:40 +0600

    feat: create modal component

---
+++ app/components/modal/RentModal.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import useRentModal from "../hooks/useRentModal";
+import CategoryInput from "../input/CategoryInput";
+import { categories } from "../navbar/Categories";
+import Modal from "./Modal";
+
+enum STEPS {
+	CATEGORY = 0,
+	LOCATION = 1,
+	INFO = 2,
+	IMAGES = 3,
+	DESCRIPTION = 4,
+	PRICE = 5,
+}
+
+const RentModal = () => {
+    const rentModal = useRentModal();
+    
+    const [step, setStep] = useState(STEPS.CATEGORY)
+    
+    const onBack = () => {
+        setStep((value) => value -1)
+    }
+
+    const onNext = () => {
+        setStep((value) => value + 1)
+    }
+
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.PRICE) {
+            return "Submit"
+        }
+        return "Next"
+    }, [step])
+    
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) {
+            return undefined
+        }
+        return "Back"
+    },[step])
+
+    let bodyContent = (
+        <div>
+            <Heading
+                title="Which of these best describes your place?"
+                subtitle="Pick a category"
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                        <CategoryInput
+                            onClick={() => { }}
+                            label={item.label}
+                            selected={false}
+                            icon={item.icon}
+                        />
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    )
+	return (
+		<div>
+			<Modal
+				isOpen={rentModal.isOpen}
+				onClose={rentModal.onClose}
+				onSubmit={rentModal.onClose}
+                actionLabel={actionLabel}
+                secondaryActionLabel={secondaryActionLabel}
+                secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+                title="Trip-Solution your home"
+                body={bodyContent}
+			/>
+		</div>
+	);
+};
+
+export default RentModal;
