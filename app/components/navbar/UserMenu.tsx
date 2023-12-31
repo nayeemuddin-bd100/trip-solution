a5bd969d0ada3d02e0b68f60d14149de9ce8603b
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2024-01-01 00:33:58 +0600

    feat: rent modal in user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "../Avatar";
-import { useState } from "react";
-import MenuItem from "./MenuItem";
-import RegisterModal from './../modal/RegisterModal';
-import useRegisterModal from "../hooks/useRegisterModal";
-import useLoginModal from "../hooks/useLoginModal";
 import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import useLoginModal from "../hooks/useLoginModal";
+import useRegisterModal from "../hooks/useRegisterModal";
+import useRentModal from "../hooks/useRentModal";
+import MenuItem from "./MenuItem";
 
 interface UserMenuProps {
 	currentUser?: User | null;
@@ -18,18 +18,29 @@ const UserMenu = ({ currentUser }: UserMenuProps) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const registerModal = useRegisterModal()
 	const loginModal = useLoginModal()
+	const rentModal = useRentModal()
+
 	const toggleOpen = () => {
 		setIsOpen((value) => !value);
 	};
+
+	
+	const onRent = useCallback(() => {
+		if (!currentUser) {
+			return loginModal.onOpen()
+		}
+
+		rentModal.onOpen()
+	},[currentUser,loginModal,rentModal])
 
 	return (
 		<div className="relative">
 			<div className="flex flex-row items-center gap-3">
 				<div
-					className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-					onClick={() => {}}
+					className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full bg-neutral-200 hover:bg-neutral-300 transition cursor-pointer"
+					onClick={onRent}
 				>
-					Your Trip Solution
+					Trip-Solution Your Home
 				</div>
 
 				<div
@@ -52,8 +63,8 @@ const UserMenu = ({ currentUser }: UserMenuProps) => {
 							<MenuItem onClick={() => {}} label="My Favorites" />
 							<MenuItem onClick={() => {}} label="My Reservation" />
 							<MenuItem onClick={() => {}} label="My Properties" />
-							<MenuItem onClick={() => { }} label="My Home" />
-							<hr/>
+							<MenuItem onClick={rentModal.onOpen} label="My Home" />
+							<hr />
 							<MenuItem onClick={() => signOut()} label="Logout" />
 						</div>
 					) : (
