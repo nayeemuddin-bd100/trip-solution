7cfa0bcb8ebcadf52b66362863b8d7b5ec223320
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2024-01-02 23:55:24 +0600

    feat: add next steps to the rent modal pop up

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -97,12 +97,23 @@ const RentModal = () => {
 			</div>
 		</div>
 	);
+
+	if(step === STEPS.LOCATION){
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading
+					title="Where is your place located?"
+					subtitle="Help guests find you!"
+				/>
+			</div>
+		)
+	}
 	return (
 		<div>
 			<Modal
 				isOpen={rentModal.isOpen}
 				onClose={rentModal.onClose}
-				onSubmit={rentModal.onClose}
+				onSubmit={onNext}
 				actionLabel={actionLabel}
 				secondaryActionLabel={secondaryActionLabel}
 				secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
