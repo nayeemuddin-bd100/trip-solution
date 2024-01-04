b24a345e51d389e13eacbab41e085870b572f670
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2024-01-05 02:07:08 +0600

    feat: complete image uploading via cloudinary

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -9,6 +9,7 @@ import useRentModal from "../hooks/useRentModal";
 import CategoryInput from "../input/CategoryInput";
 import Counter from "../input/Counter";
 import CountrySelect from "../input/CountrySelect";
+import ImageUpload from "../input/ImageUpload";
 import { categories } from "../navbar/Categories";
 import Modal from "./Modal";
 
@@ -52,6 +53,7 @@ const RentModal = () => {
 	const guestCount = watch("guestCount");
 	const roomCount = watch("roomCount");
 	const bathroomCount = watch("bathroomCount");
+	const imageSrc = watch("imageSrc");
 
 	const Map = useMemo(
 		() => dynamic(() => import("../Map"), { ssr: false }),
@@ -157,6 +159,22 @@ const RentModal = () => {
 				/>
 			</div>
 		);
+	}
+
+	if (step === STEPS.IMAGES) {
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading
+					title="Add a photo of your place"
+					subtitle="Show guests what your place looks like!"
+				/>
+
+				<ImageUpload 
+					onChange={(value) => setCustomValue('imageSrc', value)}
+					value={imageSrc}
+				/>
+			</div>
+		)
 	}
 	return (
 		<div>
