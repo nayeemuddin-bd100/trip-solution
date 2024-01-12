589c0c338552ef466b48887418e004103ac75e6e
nayeem <nayeemuddin.bd100@gmail.com>
2024-01-12 15:47:03 +0600

    feat: Create listing card layout

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,6 +6,8 @@ import useCountries from "../hooks/useCountries";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
 	data: Listing;
@@ -87,13 +89,37 @@ const ListingCard = ({
               "
 						src={data.imageSrc ? data.imageSrc : "/images/listing-alt.jpg"}
 						alt="listing image"
-          />
-          
-          {/* next day start from here */}
-          <div className="absolute top-3 right-3">
-            
-          </div>
+					/>
+
+					<div className="absolute top-3 right-3">
+						<HeartButton listingId={data.id} currentUser={currentUser} />
+					</div>
 				</div>
+
+				<div className="font-semibold text-lg">
+					{location?.region}, {location?.label}
+				</div>
+				<div className="font-light text-neutral-500">
+					{reservationDate || data.category}
+				</div>
+
+				<div className="flex flex-row items-center gap-1">
+					<div className="font-semibold">${price}</div>
+					{reservation && (
+						<div className="font-light"> night </div>
+					)}
+				</div>
+
+				{
+					onAction && actionLabel && (
+						<Button
+							disabled={disabled}
+							small
+							label={actionLabel}
+							onClick={handleCancel}
+						/>
+					)
+				}
 			</div>
 		</div>
 	);
