86f6b7b7ebf15ad9f3c664de9816f4a089656b26
nayeem <nayeemuddin.bd100@gmail.com>
2024-01-13 02:45:23 +0600

    feat: integrate favorite listing api with client

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,30 +1,37 @@
-'use client'
+"use client";
 
 import { User } from "@prisma/client";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useFavorite from "../hooks/useFavorite";
 
-interface HeartButtonProps{
-  listingId: string
-  currentUser?: User | null
-
+interface HeartButtonProps {
+	listingId: string;
+	currentUser?: User | null;
 }
 
 const HeartButton = ({ listingId, currentUser }: HeartButtonProps) => {
-  
-  const hasFavorite = false
-  const toggleFavorite = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation()
-  }
-  return <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
-    <AiOutlineHeart
-      size={28}
-      className="fill-white absolute -top-[2px] -right-[2px]"
-    />
-    <AiFillHeart
-      size={24}
-      className={hasFavorite ? 'fill-rose-500' : 'fill-neutral-500/70'}
-    />
-  </div>;
+
+  // post or delete toggle favorite listing
+	const { hasFavorite, toggleFavorite } = useFavorite({
+		listingId,
+		currentUser,
+	});
+
+	return (
+		<div
+			onClick={toggleFavorite}
+			className="relative hover:opacity-80 transition cursor-pointer"
+		>
+			<AiOutlineHeart
+				size={28}
+				className="fill-white absolute -top-[2px] -right-[2px]"
+			/>
+			<AiFillHeart
+				size={24}
+				className={hasFavorite ? "fill-rose-500" : "fill-neutral-500/70"}
+			/>
+		</div>
+	);
 };
 
 export default HeartButton;
