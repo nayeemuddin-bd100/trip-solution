63206d89d2bd2a469cb7bef9f86d9e0e57dc68cd
nayeem <nayeemuddin.bd100@gmail.com>
2024-01-21 09:33:54 +0600

    feat: complete creating reservation

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,32 +3,34 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservation from "@/app/actions/getReservations";
 
 interface IParams {
-  listingId?: string
+	listingId?: string;
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-  
-  const listing = await getListingById(params)
-  const currentUser = await getCurrentUser()
+	const listing = await getListingById(params);
+	const reservations = await getReservation(params);
+	const currentUser = await getCurrentUser();
 
-  if (!listing) {
-    return (
-      <ClientOnly>
-        <EmptyState/>
-      </ClientOnly>
-    )
-  }
+	if (!listing) {
+		return (
+			<ClientOnly>
+				<EmptyState />
+			</ClientOnly>
+		);
+	}
 
-    return (
-        <ClientOnly>
-            <ListingClient
-                listing={listing}
-                currentUser={currentUser}
-            />
-        </ClientOnly>
-    );
-}
+	return (
+		<ClientOnly>
+			<ListingClient
+				listing={listing}
+				currentUser={currentUser}
+				reservations={reservations}
+			/>
+		</ClientOnly>
+	);
+};
 
 export default ListingPage;
