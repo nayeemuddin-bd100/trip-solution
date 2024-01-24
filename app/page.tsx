3dc8db24a96623f675f5ce577915ac74f1c6971d
nayeem <nayeemuddin.bd100@gmail.com>
2024-01-24 12:05:40 +0600

    fix: dynamic server in production solve

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
+export const dynamic = "force-dynamic";
 import getCurrentUser from "./actions/getCurrentUser";
-import { IListingsParams, getListing } from "./actions/getListings";
+import { IListingsParams } from "./actions/getListings";
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-
 interface HomeProps {
-  searchParams: IListingsParams
-};
+	searchParams: IListingsParams;
+}
 
 const Home = async ({ searchParams }: HomeProps) => {
-	const listings = await getListing(searchParams);
-	const currentUser = await getCurrentUser()
+	const listings = await getListings(searchParams);
+	const currentUser = await getCurrentUser();
 
 	if (!listings || listings.length === 0) {
 		return (
@@ -26,13 +27,13 @@ const Home = async ({ searchParams }: HomeProps) => {
 		<ClientOnly>
 			<Container>
 				<div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
-						{listings.map((listing: any) => (
-              <ListingCard
-                key={listing.id}
-                data={listing}
-                currentUser={currentUser}
-              />
-						))}
+					{listings.map((listing: any) => (
+						<ListingCard
+							key={listing.id}
+							data={listing}
+							currentUser={currentUser}
+						/>
+					))}
 				</div>
 			</Container>
 		</ClientOnly>
